a8ad3d241e8fb35545b81f7af0f49932e666fa28
onisimm <onisim_roman@yahoo.com>
2025-04-04 09:56:04 +0300

    add media query for screen width checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import './App.css';
+import SkipList from './components/skip/skip-list';
 import { StepList } from './components/step/step-list';
 import Description from './components/text/description';
 import Title from './components/text/title';
+import { defaultSkips } from './constants/default-skips';
 import headerSteps from './constants/header-steps';
 import { useGetSkips } from './services/skip-service';
 
 function App() {
   const { data: skips } = useGetSkips();
 
+  // If the skips are not loaded (yet), use the default skips as placeholder
+  const skipsToRender = skips && skips.length > 0 ? skips : defaultSkips;
+
   return (
     <div className="app">
-      <StepList steps={headerSteps} currentStep={2} />
+      <div className="padding-container">
+        <StepList steps={headerSteps} currentStep={2} />
 
-      <div className="text-container">
-        <Title>Choose Your Skip Size</Title>
-        <Description>
-          Select the skip size that best suits your needs
-        </Description>
+        <div className="text-container">
+          <Title>Choose Your Skip Size</Title>
+          <Description>
+            Select the skip size that best suits your needs
+          </Description>
+        </div>
+
+        <SkipList skips={skipsToRender} />
       </div>
     </div>
   );
