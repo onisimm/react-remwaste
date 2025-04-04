670e16b56ca6823778b3c2ab9dbb50bfe17c480a
onisimm <onisim_roman@yahoo.com>
2025-04-04 12:28:35 +0300

    add information button

--- src/components/skip/skip.tsx
+++ src/components/skip/skip.tsx
@@ -1,7 +1,9 @@
 import { Cross } from '../../icons/cross';
+import { Information } from '../../icons/information';
 import { Tick } from '../../icons/tick';
 import { Skip } from '../../types/skips';
 import { getWeeklyPrice } from './skip-list';
+import { useState } from 'react';
 
 function SkipItem({
   skip,
@@ -13,12 +15,46 @@ function SkipItem({
   onSelect: () => void;
 }) {
   const { size, hire_period_days, allows_heavy_waste, allowed_on_road } = skip;
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+
+  const bagsEquivalent = calculateBagsForSkipSize(size);
+
+  const showTooltip = (e: React.MouseEvent) => {
+    e.stopPropagation(); // Prevent the skip from being selected when clicking the info icon
+    setTooltipVisible(true);
+  };
+
+  const hideTooltip = () => {
+    setTooltipVisible(false);
+  };
 
   return (
     <div className={`skip ${isSelected ? 'selected' : ''}`} onClick={onSelect}>
       <div className="skip-header">
-        <h2>{size} Yard Skip</h2>
-        <div className="description">{hire_period_days} days hire period</div>
+        <div className="skip-main-details">
+          <h2>{size} Yard Skip</h2>
+          <div className="description">{hire_period_days} days hire period</div>
+        </div>
+
+        <div
+          className="info-icon-container"
+          onMouseEnter={showTooltip}
+          onMouseLeave={hideTooltip}
+          onClick={showTooltip}>
+          <Information size={32} />
+
+          <div className={`info-tooltip ${tooltipVisible ? 'visible' : ''}`}>
+            <div className="info-tooltip-title">Skip Capacity</div>
+
+            <div className="info-tooltip-content">
+              A {size} yard skip is equivalent to approximately:
+              <div className="bag-info">
+                <span className="bag-info-number">{bagsEquivalent}</span>
+                <span>standard waste bags</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="price-container">
@@ -41,6 +77,10 @@ function Feature({ enabled, label }: { enabled: boolean; label: string }) {
       <span>{label}</span>
     </div>
   );
+}
+
+function calculateBagsForSkipSize(skipSize: number) {
+  return 5 * skipSize;
 }
 
 export default SkipItem;
